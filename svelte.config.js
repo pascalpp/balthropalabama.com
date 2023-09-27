a826cdecc9a0e8818117b5cb5cba43a830a899c6
Pascal <pascal@pascal.com>
2023-09-27 01:12:38 -0400

    add autoprefixer, update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,19 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import sveltePreprocess from 'svelte-preprocess';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+import autoprefixer from 'autoprefixer';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
+  preprocess: [
+    sveltePreprocess({
+      postcss: {
+        plugins: [autoprefixer],
+      },
+    }),
+  ],
 
   kit: {
     adapter: adapter(),
