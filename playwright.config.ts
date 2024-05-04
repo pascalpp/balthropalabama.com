302d4400068510251b00a25a8c33fbae0b31a51d
Pascal <pascal@pascal.com>
2024-05-04 13:24:29 -0400

    update playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,32 @@
-import type { PlaywrightTestConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
-const config: PlaywrightTestConfig = {
-  webServer: {
-    command: 'npm run build && npm run preview',
-    port: 4173,
-  },
+const ciBaseURL = 'http://localhost:4173';
+const productionBaseURL = 'https://www.balthropalabama.com';
+const developmentBaseURL = 'https://balthropalabama.localhost';
+const localBaseURL = process.env.PRODUCTION ? productionBaseURL : developmentBaseURL;
+const baseURL = process.env.CI ? ciBaseURL : localBaseURL;
+
+export default defineConfig({
+  webServer: process.env.CI
+    ? {
+        command: 'npm run build && npm run preview',
+        port: 4173,
+      }
+    : undefined,
   testDir: 'tests',
-};
 
-export default config;
+  use: {
+    baseURL,
+  },
+
+  projects: [
+    {
+      name: 'chrome',
+      use: { ...devices['Desktop Chrome'], deviceScaleFactor: 2 },
+    },
+    {
+      name: 'safari',
+      use: { ...devices['Desktop Safari'], deviceScaleFactor: 2 },
+    },
+  ],
+});
